6493a5207e8f298be66c8898c8676510fabc36f7
Aurélio Castro <aurelio@acos-services.tech>
2024-01-20 11:44:11 -0300

    Fix URL API

--- src/components/listPlayer.tsx
+++ src/components/listPlayer.tsx
@@ -4,7 +4,7 @@ import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover
 
 
 async function getPlayer() {
-    const res = await fetch(`https://babadosamigo.acos-services.tech/api/user`)
+    const res = await fetch(`${process.env.NEXTAUTH_URL}/api/user`)
 
     if (!res.ok) {
         throw new Error('Failed to fetch data')
@@ -15,6 +15,10 @@ async function getPlayer() {
 
 const ListPlayer = async () => {
     const users = await getPlayer()
+
+    if(!users){
+        return
+    }
 
     return (
         <div className="space-y-4 text-center">
